7595bce75d0b2bcd9227147bb2eecd6fdf45d763
seunghee <seunghee@bu.edu>
2019-05-19 21:27:01 -0400

    modification

--- main.py
+++ main.py
@@ -1,54 +1,161 @@
 import os
-from Subprocess import convert_video_720
-from Subprocess import convert_video_480
-import test_files as test
+#from Subprocess import *
+# from Subprocess import convert_video_480
 import threading
-import queue
 import time
+import subprocess
+import os
+import json
+import multiprocessing as mp
+import queue
+from pytest import approx
 
+
+
+"""
+input files to the queue
+return finalized Q
+"""
+def fetch_files(inputpath):  
+
+	Vid_Q = queue.Queue() 
+
+	vid_requests_path = inputpath
+	files = os.listdir(vid_requests_path)
+
+	for video in files:
+		# detect file type (MP4)
+		extension = video.split('.')
+		if ( extension[-1] == 'mp4' or  
+			extension[-1] == '.avi' or 
+			extension[-1] == '.flv' or 
+			extension[-1] == '.wmv' or 
+			extension[-1] == '.mov'):
+				Vid_Q.put(video)
+
+		else:
+			pass
+
+	return Vid_Q
+
+
+
+def convert_video_720(inputpath):
+	 # ffmpeg -i input.wmv -s hd720 -c:v libx264 -crf 23 -c:a aac -strict -2 output.mp4
+
+		if not os.path.exists('./output_videos/'):  # create output folder
+			os.mkdir('./output_videos/')
+		else:
+		    outputpath = "./output_videos/"+inputpath[:-4]+"_720p.mp4"
+		    cmds = ['ffmpeg', '-i', './videos/'+ inputpath , '-r','30','-s','hd720',outputpath]
+		    conversion1 = subprocess.Popen(cmds)
+		    #init_counter = init_counter+1
+		    #counter_720 = counter_720+1
+
+		    wait1 = conversion1.wait()
+		    #done_counter = done_counter+1
+
+		#if(init_counter != done_counter):
+		#	print("error occurred the initial file is" + init_counter+ "the finished task is " + done_counter)
+		
+		#else:
+		#	print("Finished" + counter_720 +  "Video720\n")
+	
+
+def convert_video_480(inputpath):
+
+		if not os.path.exists('./output_videos/'):  # create output folder
+			os.mkdir('./output_videos/')
+
+		else:    
+			outputpath = "./output_videos/"+inputpath[:-4]+"_480p.mp4"
+			cmds = ['ffmpeg', '-i','./videos/'+ inputpath, '-r','30','-s','hd480',outputpath]
+			conversion2 = subprocess.Popen(cmds)
+			#init_counter = init_counter+1
+			#counter_480 = counter_480+1
+
+			wait2 = conversion2.wait()
+			#done_counter = done_counter+1
+
+		#if(init_counter != done_counter):
+		#	print("error occurred the initial file is" + init_counter+ "the finished task is " + done_counter)
+		
+		#else:
+		#	print("Finished" + counter_480 +  "Video480\n")
+
+
+
+def Threading(Vid_Q):
+	if(Vid_Q.empty()): #no videos
+		print("No Files to Convert")
+		pass
+	else:
+
+		#while the queue has video files run convert until nofiles left
+		while (not Vid_Q.empty()):
+
+			video = Vid_Q.get()
+
+			#one thread per one resolution 
+
+			convert_480 = threading.Thread(target=convert_video_480, args=(video,)) # convert to 480
+			convert_480.start()
+			convert_720 = threading.Thread(target=convert_video_720, args=(video,)) # convert to 720
+			convert_720.start()
+
+			counvert_480.join()
+			convert_720.join()
+
+
+	#print(str(counter) + "files are converting in total")
+
+
+
+
+# def ffprobe(inputfile,outputfile):
+# 	input_info = subprocess.check_output(['ffprobe', '-v', 'warning',
+#                                     '-print_format', 'json',
+#                                     '-show_streams',
+#                                     '-show_format',
+#                                     inputfile],text=True)
+# 	input_data = json.loads(input_info)
+
+# 	output_info = subprocess.check_output(['ffprobe', '-v', 'warning',
+#                                     '-print_format', 'json',
+#                                     '-show_streams',
+#                                     '-show_format',
+#                                     outputfile],text=True)
+# 	output_data = json.loads(output_info)
+
+# 	ori_duration = float(input_data['streams'][0]['duration'])
+# 	out_duration = float(output_data['streams'][0]['duration'])
+
+# 	assert ori_duration == approx(out_duration)
 
 
 def main():
 
-
-	q1 = queue.Queue()
-	q2 = queue.Queue()
-
-	inputPath = "/Users/leeseunghee/Documents/Exercise2/videos"
-	video_input1 =""
-	video_input2 =""
-
-	files = os.listdir(inputPath)
-
-	for file in files:
-		q1.put(file)
-	while not q1.empty():
-		video_input1 = q1.get()
-
-	print("before",files)
-
-	for file in files:
-		q2.put(file)
-	while not q2.empty():
-		video_input2 = q2.get()
+	inputPath = "./videos"
+	input_q = fetch_files(inputPath)
 
 
-	video_output1 = "result720.mp4"
-	video_output2 = "result420.mp4"
 
 
-	t1 = threading.Thread(target = convert_video_720, args = (video_input1,video_output1))
-	t2 = threading.Thread(target = convert_video_480, args = (video_input2,video_output2))
+	init_counter = 0
+	done_counter = 0
+	counter_480=0
+	counter_720 =0
+	outputPath_480 =''
+	outputPath_720 = ''
 
-	t1.start()
-	t2.start()
+	#START Threading
+	conversion = threading.Thread(target=Threading, args =(input_q,))
+
+	conversion.start()
 
 
-	# dur1 = test.test_duration("full_ball.mp4")
-	# dur2 = test.test_duration("result720.mp4")
-
-	# test.asserting(t1[0],t2[0])
 
 
 if __name__ == '__main__':
 	main()
+
