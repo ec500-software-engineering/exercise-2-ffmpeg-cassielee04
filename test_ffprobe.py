41a09999f5d59c6ddbecd98ab4b09e3bccaa5d75
seunghee <seunghee@bu.edu>
2019-05-20 03:15:38 -0400

    subprocess changed2

--- test_ffprobe.py
+++ test_ffprobe.py
@@ -17,21 +17,27 @@ from pathlib import Path
 def test_duration():
 
 
-	orig_vid = 'full_ball.mp4'
-	convert_480 = 'full_ball_480p.mp4'
+	orig_vid = './full_ball.mp4'
+	convert_480 = './full_ball_480p.mp4'
 	#convert_720 = './output_videos/full_ball_720p.mp4'
 
 
-	orig_probe =subprocess.check_output(['ffprobe', '-v', 'warning',
+	orig_probe = json.loads(subprocess.check_output(['ffprobe', '-v', 'warning',
                                     '-print_format', 'json',
                                     '-show_streams',
                                     '-show_format',
-                                    orig_vid])
-	#probe_480 = ffprobe(convert_480)
+                                    orig_vid]))
+	probe_480 = json.loads(subprocess.check_output(['ffprobe', '-v', 'warning',
+                                    '-print_format', 'json',
+                                    '-show_streams',
+                                    '-show_format',
+                                    convert_480]))
 	#probe_720 = ffprobe(convert_720)
 
+	#result = subprocess.Popen(["ffprobe", "full_ball.mp4"],stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
+
 	ori_duration = float(orig_probe['streams'][0]['duration'])
-	#duration_480 = float(probe_480['streams'][0]['duration'])
+	duration_480 = float(probe_480['streams'][0]['duration'])
 	#duration_720 = float(probe_720['streams'][0]['duration'])
 
 	return ori_duration
