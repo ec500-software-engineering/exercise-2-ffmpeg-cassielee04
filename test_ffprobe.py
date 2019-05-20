4273efb4a37b13ea42726100f7738e905d355d58
seunghee <seunghee@bu.edu>
2019-05-20 02:16:02 -0400

    subprocess changed

--- test_ffprobe.py
+++ test_ffprobe.py
@@ -4,26 +4,34 @@ import subprocess
 from pathlib import Path
 
 
-def ffprobe(file:Path)->dict:
-	input_info = subprocess.check_output(['ffprobe', '-v', 'warning',
-                                    '-print_format', 'json',
-                                    '-show_streams',
-                                    '-show_format',
-                                    file],universal_newlines=True)
-	input_data = json.loads(input_info)
-	return input_data
+# def ffprobe(file:Path)->dict:
+# 	input_info = subprocess.check_output(['ffprobe', '-v', 'warning',
+#                                     '-print_format', 'json',
+#                                     '-show_streams',
+#                                     '-show_format',
+#                                     file],universal_newlines=True)
+# 	input_data = json.loads(input_info)
+# 	return input_data
 
 
 def test_duration():
 
 
-	orig_vid = 'full_ball.mp4'
-	convert_480 = 'full_ball_480p.mp4'
+	orig_vid = './videos/full_ball.mp4'
+	convert_480 = './output_videos/full_ball_480p.mp4'
 	#convert_720 = './output_videos/full_ball_720p.mp4'
 
 
-	orig_probe = ffprobe(orig_vid)
-	probe_480 = ffprobe(convert_480)
+	orig_probe = json.loads(subprocess.check_output(['ffprobe', '-v', 'warning',
+                                    '-print_format', 'json',
+                                    '-show_streams',
+                                    '-show_format',
+                                    orig_vid],universal_newlines=True))
+	probe_480 = json.loads(subprocess.check_output(['ffprobe', '-v', 'warning',
+                                    '-print_format', 'json',
+                                    '-show_streams',
+                                    '-show_format',
+                                    convert_480],universal_newlines=True))
 	#probe_720 = ffprobe(convert_720)
 
 	ori_duration = float(orig_probe['streams'][0]['duration'])
